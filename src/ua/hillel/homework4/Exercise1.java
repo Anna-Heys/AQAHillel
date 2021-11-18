918990f2ddad91b0b8114df02f069d8b54583165
PRIME1\anna.trukhina <ann88ks@gmail.com>
2021-11-18 16:55:15 +0200

    hw4 changes

--- src/ua/hillel/homework4/Exercise1.java
+++ src/ua/hillel/homework4/Exercise1.java
@@ -3,7 +3,7 @@ package ua.hillel.homework4;
 public class Exercise1 {
     public static void main(String[] args) {
 
-    //some changes
+    //Написать программу, которая печатает таблицу значений sin(x), где x это каждые 10 градусов от 0 до 360
 
     }
 }
--- src/ua/hillel/homework4/Exercise2.java
+++ src/ua/hillel/homework4/Exercise2.java
@@ -2,6 +2,13 @@ package ua.hillel.homework4;
 
 public class Exercise2 {
     public static void main(String[] args) {
-   //some changes
+
+
+   //Написать программу, которая в массиве из случайных чисел заменяет все чётные числа на 0 и выводит полученный массив на экран.
+        // Для генерации случайных чисел можно использовать Random:
+        //Random random = new Random;
+        //int i = random.nextInt(100); //случайное целое число до 100
+
+
     }
 }
